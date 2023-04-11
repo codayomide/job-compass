1e5224ad25b7a951752123271f3279fb990382f4
RalphFred <111363603+RalphFred@users.noreply.github.com>
2023-04-11 11:56:12 +0100

    Worked on the basic outline on the homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import{ Route, Routes, Link} from 'react-router-dom'
+import './assets/styles/css/style.css';
+import Logo from './assets/logo.png';
+import Home from './Home';
+import SignUp from './SignUp';
 
 function App() {
+
+  const handleToggle = () => {
+    console.log("ITs working")
+  }
+
   return (
-    <div className="app">
-    
+    <div className="App">
+
+      <nav className="nav--wrapper">
+        <div className="nav--wrapper__logo">
+          <div className="menu-btn" onClick={handleToggle}>
+              <span className="menu-btn__bar"></span>
+          </div>
+          <Link to="/home" ><img src={Logo} alt="" /></Link>
+        </div>
+
+        <ul className="nav--wrapper__links mobile">
+          <li className="link">Categories</li>
+          <li className="link">Find Job</li>
+          <li className="link"> <Link to="/Blog" >Blog</Link> </li>
+          <li className="link"> <Link to="/SignUp" >Sign Up</Link> </li>
+        </ul>
+      </nav>
+
+      
+    <Routes>
+      <Route path="/home" element={ <Home/> } />
+      <Route path="/SignUp" element={ <SignUp/> } />
+    </Routes>
     </div>
   );
 }
