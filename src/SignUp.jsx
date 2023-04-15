fc22a835fdf4bce8e81439a57807483d93268c3e
codayomide <smokayomide@gmail.com>
2023-04-15 14:44:37 +0100

    editted sign up form

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -12,13 +12,36 @@ const SignUp = () => {
 
         <div className="form-main">
           <div className="input-wrapper">
-            <label htmlFor=""></label>
-            <input type="text" placeholder="Your email" />
+            <label htmlFor="email"></label>
+            <input
+              type="text"
+              placeholder="Email"
+              {...register("email", { required: true, pattern: /^(?=[^\s@]{1,64}@)[^\s@]+@[^\s@]+\.[^\s@.]+$/ })}
+              aria-invalid={errors.email ? "true" : "false"}
+            />
+            {errors.email && <p role="alert">Invalid Email</p>}
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="username"></label>
+            <input
+              type="text"
+              placeholder="Username"
+              {...register("username", { required: true, pattern: "^[A-Za-z][A-Za-z0-9_]{7,29}$" ,minLength: 8, maxLength: 256})}
+              aria-invalid={errors.username ? "true" : "false"}
+            />
+            {errors.username && <p role="alert">Invalid Username</p>}
           </div>
           
           <div className="input-wrapper">
-            <label htmlFor=""></label>
-            <input type="text" placeholder="Your password" />
+            <label htmlFor="password"></label>
+            <input
+              type="text"
+              placeholder="Password"
+              {...register("password", { required: true, minLength: 8, maxLength: 256})}
+              aria-invalid={errors.password ? "true" : "false"}
+            />
+            {errors.password && <p role="alert">Invalid Password</p>}
           </div>
           
           <button className="submit-btn">Sign Up</button>
