e5fce07221d5ae974b70d272cd551b30104d3abc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-06-03 03:25:28 +0100

    made changes: 'improved on the auth UI'

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,64 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import Navbar from '../App/Navbar';
+import { useForm } from "react-hook-form";
+import { useState, useEffect } from 'react';
+import Navbar from "../App/Navbar";
 
 const Login = () => {
-  const [token, setToken] = useState(null);
-  const [loginErrors, setLoginErrors] = useState("");
-  const { register, handleSubmit, formState: { errors } } = useForm();
+    const [token, setToken] = useState(null);
+    const [loginErrors, setLoginErrors] = useState("");
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    if (storedToken) {
-      setToken(storedToken);
-    }
-  }, []);
-   
-  const onFormSubmit = (data) => {
-    fetch('https://jobcompass.cyclic.app/api/auth/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      const token = data.accessToken;
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        if (storedToken) {
+          setToken(storedToken);
+        }
+    }, []);
+     
+    const onFormSubmit = (data) => {
+        fetch('https://jobcompass.cyclic.app/api/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            const token = data.accessToken;
 
-      console.log(token)
+            localStorage.setItem('token', token);
 
-      localStorage.setItem('token', token);
+            setToken(token);
 
-      setToken(token);
+            window.location.href = '/dashboard';
 
-    //   window.location.href = '/dashboard';
+            setLoginErrors(data.message);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
 
-      setLoginErrors(data.message);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  };
+    return ( 
+        <div id="login-page">
+            <Navbar />
+            <form className="form" onSubmit={handleSubmit(onFormSubmit)}>
+            <h2>Log in</h2>
 
-  return (
-    <div className="login-container">
-      <Navbar />
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit(onFormSubmit)}>
-        <div className="form-group">
-          <label>Email</label>
-          <input {...register('email', { required: true })} />
-          {errors.email && <span className="error-message">Email is required</span>}
+            <div className="form-main">
+                <div className="input-wrapper">
+                    <label htmlFor="userlogin"></label>
+                    <input
+                    type="text"
+                    placeholder="Username"
+                    {...register("username", {
+                        required: 'This field is required', 
+                        pattern: {
+                            value: /(?!.*\s)[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*\.[A-Za-z]{2,}$|^(?!.*\s)[A-Za-z0-9]+([._-][A-Za-z0-9]+)*/
+                            ,
+                            message: 'Please enter a valid email or username'
+                        },
+                        minLength: {
+                            value: 6,
+                            message: 'Please enter a valid email or username'
+                        }
+                    })}
+                    aria-invalid={errors.username ? "true" : "false"}
+                    />
+                    {errors.username && <p role="alert">{errors.username.message}</p>}
+                </div>
+        
+                <div className="input-wrapper">
+                    <label htmlFor="password"></label>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        {...register("password", {
+                            required: 'This field is required',
+                            minLength: {
+                                value: 8,
+                                message: 'Password is too short'
+                            },
+                            maxLength: {
+                                value: 60,
+                                message: 'Password is too long'
+                            }
+                        })}
+                        aria-invalid={errors.password ? "true" : "false"}
+                    />
+                    {errors.password && <p role="alert">{errors.password.message}</p>}
+                </div>
+
+                <button className="submit-btn">Log in</button>
+                
+                {loginErrors && <p role="alert">{loginErrors}</p>}
+                </div>
+            </form>
         </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input {...register('password', { required: true })} />
-          {errors.password && <span className="error-message">Password is required</span>}
-        </div>
-        {loginErrors && <span className="error-message">{loginErrors}</span>}
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
-
+    );
+}
+ 
 export default Login;
